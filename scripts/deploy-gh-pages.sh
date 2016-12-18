c6c9a7b36b65e20990cc6a61fd7c9e60c5da9738
John Brilla <jbrilla17@gmail.com>
2016-12-18 10:14:27 -0500

    adding deploy script

---
+++ scripts/deploy-gh-pages.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+git checkout -b gh-pages
+./build_all.sh
+
+rm -rf _clients
+rm -rf _site_generator
+mv _site/* .
+
+git add .
+git commit -m 'adding site'
+git push origin gh-pages
+git checkout master
+git branch -D gh-pages
+
+echo '----'
+echo 'DONE'
+echo '----'
