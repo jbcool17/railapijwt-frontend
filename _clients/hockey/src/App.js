7818635482443fc4f73272977c52218d217ec728
John Brilla <jbrilla17@gmail.com>
2017-01-17 17:20:15 -0500

    add select teams to view

--- _clients/hockey/src/App.js
+++ _clients/hockey/src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import HockeySearchView from './HockeySearchView';
 import HockeyDataView from './HockeyDataView';
-import HockeyTeamsView from './HockeyTeamsView';
 import sortBy from './sortHelpers';
 
 var url = window.location.hostname === 'localhost' ? 'http://localhost:3000/v1' : 'https://floating-tor-40582.herokuapp.com/v1';
@@ -12,13 +11,27 @@ class App extends Component {
 
         this.state = {
             data: [],
-            info: ''
+            info: '',
+            teams: []
         };
 
         this.searchTeamNames = this.searchTeamNames.bind(this);
         this.hockeySort = this.hockeySort.bind(this);
         this.getAll = this.getAll.bind(this);
+        this.listChange = this.listChange.bind(this);
     };
+    componentWillMount(){
+      fetch(url + "/standings/teams", { method: 'GET' }).then(function(response) {
+          return response.json()
+      }).then(function(data) {
+          this.setState({teams: data});
+          console.log('setting data')
+
+      }.bind(this)).catch(function(error) {
+          console.log('ERROR')
+          console.log(error);
+      });
+    }
     searchTeamNames(e) {
         this.setState({info: '- Looking up teams...'})
         var teamName = e.target.value.trim();
@@ -124,15 +137,58 @@ class App extends Component {
           console.log(error);
       });
     }
+    listChange(e){
+      var index = e.nativeEvent.target.selectedIndex,
+          teamName = encodeURI(e.nativeEvent.target[index].text);
+      fetch(url + "/standings/teams?name=" + teamName, { method: 'GET' }).then(function(response) {
+          return response.json()
+      }).then(function(json) {
+          var data = json.data,
+              hockeyData = [];
+
+          for (var i = 0; i < data.length; i++) {
+              hockeyData.push({ id: data[i].id, team_name: data[i].attributes['team-name'],
+                                games: data[i].attributes.games,
+                                wins: data[i].attributes.wins,
+                                losses: data[i].attributes.losses,
+                                losses_ot: data[i].attributes['losses-ot'],
+                                points: data[i].attributes.points
+                             })
+          }
+
+          if (hockeyData.length > 0) {
+              console.log('Setting Hockey Data: ')
+              console.log(hockeyData);
+              this.setState({ data: hockeyData, info: "- "+ hockeyData.length + " Team(s) Loaded..." })
+              document.getElementById("hockey-table").style.visibility = "";
+
+          } else {
+              document.getElementById("hockey-table").style.visibility = "hidden";
+              var tableHeaders = document.getElementsByTagName('th');
+              for (var k = 0; k < tableHeaders.length; k++){
+                tableHeaders[k].style.background = '';
+              }
+              this.setState({info: '- Not Found.'})
+          }
+
+      }.bind(this)).catch(function(error) {
+          console.log('ERROR')
+          console.log(error);
+      });
+
+    }
     render() {
 
         return (
             <div className="Hockey">
               <div className='title'>
                 <h2>Hockey API</h2>
+                <p>Data Updated Daily</p>
               </div>
-              <HockeyTeamsView />
-              <HockeySearchView getAll={this.getAll} onKeyUp={this.searchTeamNames}/>
+              <HockeySearchView listChange={this.listChange}
+                                teams={this.state.teams}
+                                getAll={this.getAll}
+                                onKeyUp={this.searchTeamNames} />
               <HockeyDataView data={this.state.data} info={this.state.info} hockeySort={this.hockeySort}/>
             </div>
         );
--- _clients/hockey/src/HockeySearchView.js
+++ _clients/hockey/src/HockeySearchView.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class HockeySearchView extends Component {
-
   render() {
+    var teams = this.props.teams.map(function(team){
+      return (<option key={team} id={team}>{team}</option>)
+    });
     return (
       <div className="HockeySearchView">
           <h2>Hockey Search</h2>
           <input onKeyUp={this.props.onKeyUp} type="text" id="teamNames" placeholder="Enter Team Name"/>
           <button onClick={this.props.getAll}>Get All</button>
+          <select onChange={this.props.listChange} id='teamList'>
+            <option>Get All Dates for Specific Team</option>
+            {teams}
+          </select>
       </div>
     );
   }
